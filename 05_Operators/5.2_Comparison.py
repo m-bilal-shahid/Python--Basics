7c2afd116680ac244048effc53ec55823180a9c3
Muhammad Bilal Shahid <shkbilalshahid@gmail.com>
2025-10-04 18:37:05 +0500

    Create 5.2_Comparison.py

---
+++ 05_Operators/5.2_Comparison.py
@@ -0,0 +1,41 @@
+
+# Comparison Operators Are Used To Compare Two Values
+# They Return True or False Based On The Condition
+# They Are Also Called Relational Operators
+
+# 1. == (Equal To):
+#    Returns True if both values are equal.
+# print(5 == 5)   # True
+
+# 2. != (Not Equal To):
+#    Returns True if values are not equal.
+# print(5 != 3)   # True
+
+# 3. > (Greater Than):
+#    Returns True if left value is greater.
+# print(10 > 5)   # True
+
+# 4. < (Less Than):
+#    Returns True if left value is smaller.
+# print(3 < 7)    # True
+
+# 5. >= (Greater Than or Equal To):
+#    Returns True if left value is greater or equal.
+# print(5 >= 5)   # True
+
+# 6. <= (Less Than or Equal To):
+#    Returns True if left value is smaller or equal.
+# print(4 <= 6)   # True
+
+
+# Example Program:
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+print("Equal To (==):", a == b)
+print("Not Equal To (!=):", a != b)
+print("Greater Than (>):", a > b)
+print("Less Than (<):", a < b)
+print("Greater Than or Equal To (>=):", a >= b)
+print("Less Than or Equal To (<=):", a <= b)
